f46e3668be19e0bc3d7672b3f63f563b275ae829
Justin Hammond <justin.hammond@rackspace.com>
2013-01-13 16:24:47 -0600

    Moved to cmake project

---
+++ gauntcraft/gauntcraft/src/CoreEntity.cpp
@@ -0,0 +1,33 @@
+#include "CoreEntity.h"
+
+CoreEntity::CoreEntity() {
+
+}
+
+CoreEntity::~CoreEntity() {
+
+}
+
+void CoreEntity::handleInput(SDL_Event * event) {
+
+}
+
+void CoreEntity::moveTo(CORE_INT newX, CORE_INT newY) {
+	x = newX;
+	y = newY;
+}
+
+void CoreEntity::move(CORE_UINT32 deltaTicks) {
+	CORE_FLOAT xCalcVel = (CORE_FLOAT)xVel * (deltaTicks / 1000.f);
+	CORE_FLOAT yCalcVel = (CORE_FLOAT)yVel * (deltaTicks / 1000.f);
+	x += (CORE_INT) xCalcVel;
+	y += (CORE_INT) yCalcVel;	
+}
+
+CORE_INT CoreEntity::getX() {
+	return x;
+}
+
+CORE_INT CoreEntity::getY() {
+	return y;
+}
